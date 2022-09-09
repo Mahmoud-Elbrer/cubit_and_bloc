3b60d12354b2fe3be7ca3afce189631c7c00a228
mahmudandroid <mnorelhadi@gmail.com>
2022-09-09 23:48:46 +0400

    add cubit and flutter_bloc packages

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cubit_and_bloc/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CounterScreen(title: 'Cubit Demo'),
+    );
+  }
+}
+
